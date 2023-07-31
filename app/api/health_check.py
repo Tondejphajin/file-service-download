f91496e4032e378b430d37ec6f2a8b743339bb54
ploychanok jaleansook <toonploychanok@gmail.com>
2023-07-31 17:54:20 +0700

    insert token on health check's url

--- app/api/health_check.py
+++ app/api/health_check.py
@@ -14,7 +14,6 @@ def check_minio() -> bool:
         raise HTTPException(status_code=500, detail="MinIO not available")
     return True
 
-
 def check_redis() -> bool:
     try:
         redis_client = RedisClient()
@@ -22,7 +21,6 @@ def check_redis() -> bool:
     except:
         raise HTTPException(status_code=500, detail="Redis not available")
     return True
-
 
 def check_worker() -> bool:
     worker_file_name = "worker"
@@ -34,10 +32,9 @@ def check_worker() -> bool:
         raise HTTPException(status_code=500, detail="Celery worker not available")
     return True
 
-
-def patch_health_check():
-    url = f"{env.get_value('APP_REGISTER_DOMAIN')}/appapi/v1/health-check?appid={env.get_value('APP_ID')}"
-    response = requests.patch(url=url)
+def patch_health_check(payload:dict):
+    url = f"{env.get_value('APP_REGISTER_DOMAIN')}/appapi/v1/health-check?appid={env.get_value('APP_ID')}&token={env.get_value('TOKEN')}"
+    response = requests.patch(url=url, json=payload)
     print(response.text)
     print(response.json())
     print(response.status_code)
@@ -47,9 +44,8 @@ def patch_health_check():
 
     return response.json()["data"]["is_online"]
 
-
 def get_health_check():
-    url = f"{env.get_value('APP_REGISTER_DOMAIN')}/appapi/v1/health-check?appid={env.get_value('APP_ID')}"
+    url = f"{env.get_value('APP_REGISTER_DOMAIN')}/appapi/v1/health-check?appid={env.get_value('APP_ID')}&token={env.get_value('TOKEN')}"
     response = requests.get(url=url)
     print(response.text)
     print(response.json())
@@ -58,24 +54,25 @@ def get_health_check():
     if response.status_code != 200:
         raise HTTPException(status_code=500, detail="Server is not available")
 
-    return response.json()["data"]["is_online"]
-
+    return response.json()[0]
 
 @router.get("/services")
 async def get_services_status():
     if check_minio() and check_redis() and check_worker():
         return {"status": "all services are online"}
 
-
 @router.get("/")
-async def get_health_check():
+async def get_health_check_record():
     get_response = get_health_check()
     return {"status": get_response}
 
-
 @router.patch("/")
-async def update_health_check():
+async def update_health_check_record():
     get_response = get_health_check()
-    if get_response != "True":
-        patch_response = patch_health_check()
+    if get_response["data"]["is_online"] != "True":
+
+        # update health check status
+        get_response["data"]["is_online"] = True
+
+        patch_response = patch_health_check(get_response)
         return {"status": patch_response}
