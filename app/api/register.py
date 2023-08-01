560326b45514f483c6b27403c0edf969deb7bc85
ploychanok jaleansook <toonploychanok@gmail.com>
2023-08-01 11:57:14 +0700

    validate, update and fix bug
    - wrong url path
    - parse parameter when new register on app-register
    - update some of confusing between get_value and check_key_and_value_exist method

--- app/api/register.py
+++ app/api/register.py
@@ -8,65 +8,57 @@ env = Env()
 
 
 def post_app_registration() -> tuple[str, str]:
-    url = f"{env.get_value('APP_REGISTER_DOMAIN')}/appapi/app"
-    print(url)
-
+    url = f"{env.get_value('APP_REGISTER_DOMAIN')}/apps/api/create"
     app_id, token = None, None
 
     payload = {
         "name": "file_service",
-        "scope": "readWrite",
+        "scope": "readwrite",
         "service_type": "port",
         "data": "8000",
         "auth_type": "bypass",
     }
 
-    response = requests.post(url=url, json=payload)
-
-    print(response.text)
-    print(response.json())  # text -> json
-    print(response.status_code)
+    response = requests.post(url=url, json=payload)    
 
     if response.status_code == 200:
-        app_id = response.json()["data"]["authen"]["app_id"]
+        app_id = response.json()["data"]["authen"]["appid"]
         token = response.json()["data"]["authen"]["token_key"]
     return app_id, token
 
-
 def get_app_registration_info():
-    url = f"{env.get_value('APP_REGISTER_DOMAIN')}/appapi/v1/app"
+    url = f"{env.get_value('APP_REGISTER_DOMAIN')}/appapi/v1/app/{env.get_value('APP_ID')}?token={env.get_value('TOKEN')}"
 
     response = requests.get(url=url)
-    print(response.text)
-    print(response.json())
-    print(response.status_code)
-
+    
     if response.status_code == 200:
-        return response.json()["data"]
-
+        return response.json()
 
 @router.post("/")
-async def register():
+def register():
+    
     if env.check_key_and_value_exist("APP_ID") and env.check_key_and_value_exist(
         "TOKEN"
-    ):
-        return {"registration_info": "already registered"}
+    ):  
+        app_id =  env.get_value("APP_ID")
+        token =  env.get_value("TOKEN")
+        
+        # maybe error if appid and bypass token are not available or already update.
+        return {"registration_info": "already registered", "data":{"app_id":app_id, "token":token}}
     else:
         app_id, token = post_app_registration()
+        # app_id, token = 'eca94222-8fbd-42b3-8640-945a51c1ec17', 'farBKPGLU2OwaRPPm9cJx8h6b8BqCkCAgNomTmGDCrHjhkI0i5IvzCxGSoWZNVVE2tZJyZ0ExpjiVgDCQPGKzvNk7romNtGFE9SiGXjUyhhTifTH8iai8RZi'
+
         if app_id is not None and token is not None:
             env.set_key("APP_ID", app_id)
             env.set_key("TOKEN", token)
 
-            return {"registration_info": "success"}
+            return {"registration_info": "success", "data":{"app_id":app_id, "token":token} }        
         else:
             return {"registration_info": "failed"}
 
 
 @router.get("/")
-async def get_register_info():
+def get_register_info():
     data = get_app_registration_info()
     return data
-
-
-if __name__ == "__main__":
-    pass
--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from app.api import download, health_check, ticket, register, license
-from app.api import *
+import time
+
+def starter_process():
+    # check token and register app
+    result = register.register()
+    
+    if result and result['registration_info'] != "failed":
+        app_id, token = result['data']['app_id'], result['data']['token']
+        
+        time.sleep(2)
+        # get license
+        license.check_license(app_id, token)
+        # get and update health-check
+        health_check.update_health_check_record(app_id, token) 
+
+starter_process()
 
 app = FastAPI()
 
@@ -13,16 +28,9 @@ app.include_router(
 )
 app.include_router(license.router, prefix="/appapi/v1/license", tags=["license"])
 
-
 @app.get("/")
 async def root():
     return FileResponse("app/api/templates/index.html")
 
-def starter_process():
-    # check token and register app
-    register.register()
-   
-    # get license
-    license.check_license()
-    # get and update health-check
-    health_check.update_health_check_record() 
+
+
