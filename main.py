36c40e826967cf301412561a971020db97c8215d
tindpj <106011455+Tondejphajin@users.noreply.github.com>
2023-08-04 11:50:05 +0700

    Change route's prefix and add ping response

--- main.py
+++ main.py
@@ -6,31 +6,32 @@ import time
 # def starter_process():
 #     # check token and register app
 #     result = register.register()
-    
+
 #     if result and result['registration_info'] != "failed":
 #         app_id, token = result['data']['app_id'], result['data']['token']
-        
+
 #         time.sleep(2)
 #         # get license
 #         license.check_license(app_id, token)
 #         # get and update health-check
-#         health_check.update_health_check_record(app_id, token) 
+#         health_check.update_health_check_record(app_id, token)
 
 # starter_process()
 
 app = FastAPI()
 
-app.include_router(download.router, prefix="/appapi/v1/download", tags=["download"])
-app.include_router(ticket.router, prefix="/appapi/v1/ticket", tags=["ticket"])
-app.include_router(register.router, prefix="/appapi/v1/app", tags=["register"])
-app.include_router(
-    health_check.router, prefix="/appapi/v1/health-check", tags=["healthcheck"]
-)
-app.include_router(license.router, prefix="/appapi/v1/license", tags=["license"])
+app.include_router(download.router, prefix="/download", tags=["download"])
+app.include_router(ticket.router, prefix="/ticket", tags=["ticket"])
+app.include_router(register.router, prefix="/app", tags=["register"])
+app.include_router(health_check.router, prefix="/health-check", tags=["healthcheck"])
+app.include_router(license.router, prefix="/license", tags=["license"])
+
 
 @app.get("/")
 async def root():
     return FileResponse("app/api/templates/index.html")
 
 
-
+@app.get("/ping")
+async def ping():
+    return {"message": "pong!"}
