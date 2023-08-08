2fd1a4492e265aea19f5102794131f8ab3095ba5
tindpj <106011455+Tondejphajin@users.noreply.github.com>
2023-08-08 14:11:34 +0700

    Update test_redis_utils.py

--- test_redis_utils.py
+++ test_redis_utils.py
@@ -1,15 +1,48 @@
 from utils.redis_utils import RedisUtils
-import json
-
-redis_utils = RedisUtils()
-
-
-# test hget non-exist key
-def test_hget_non_exist_key():
-    result = redis_utils.client.hget("non-exist-key", "non-exist-field")
-    print(result)
+from datetime import datetime, timedelta
 
 
 if __name__ == "__main__":
-    test_hget_non_exist_key()
-    assert False
+    redis_utils = RedisUtils()
+
+    default_expired_time = datetime.utcnow() + timedelta(days=1)
+    file_path_1 = {
+        "path_name": "videos",
+        "include": ["cat.mp4", "dog.mp4"],
+        "exclude": [],
+        "expired_time": default_expired_time.isoformat(),
+        "version_id": "",
+    }
+
+    file_path_2 = {
+        "path_name": "documents",
+        "include": ["report.pdf", "hw.pdf"],
+        "exclude": [],
+        "expired_time": default_expired_time.isoformat(),
+        "version_id": "",
+    }
+
+    file_path_3 = {
+        "path_name": "pictures",
+        "include": ["qr.jpg", "me.jpg"],
+        "exclude": [],
+        "expired_time": default_expired_time.isoformat(),
+        "version_id": "",
+    }
+
+    file_path_4 = {
+        "path_name": "pictures",
+        "include": ["me.jpg", "qr.jpg"],
+        "exclude": [],
+        "expired_time": default_expired_time.isoformat(),
+        "version_id": "",
+    }
+
+    file_paths = [file_path_1, file_path_2, file_path_3]
+
+    redis_utils.cached_request_multiple_paths("test", file_paths)
+
+    file_paths_duplicate = [file_path_1, file_path_2, file_path_4]
+
+    result = redis_utils.check_cached_multiple_paths(file_paths_duplicate)
+    print(result)
