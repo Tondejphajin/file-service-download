3f54fcde6109fd51ff74a7fc9204a9a9e0d22324
tindpj <106011455+Tondejphajin@users.noreply.github.com>
2023-08-08 18:27:45 +0700

    Switch to OOP

--- test_redis_utils.py
+++ test_redis_utils.py
@@ -1,48 +1,43 @@
 from utils.redis_utils import RedisUtils
 from datetime import datetime, timedelta
+import uuid
+
+
+class TestRedisUtils:
+    def __init__(self):
+        self.redis_utils = RedisUtils()
+        self.default_expired_time = datetime.utcnow() + timedelta(days=1)
+
+        self.file_path_1 = {
+            "path_name": "videos",
+            "include": ["Go.mp4", "Slides.pdf"],
+            "exclude": [],
+            "expired_time": self.default_expired_time.isoformat(),
+            "version_id": "",
+        }
+
+        self.file_path_2 = {
+            "path_name": "documents",
+            "include": ["Cap1.1.pdf", "MD.pdf"],
+            "exclude": [],
+            "expired_time": self.default_expired_time.isoformat(),
+            "version_id": "",
+        }
+
+        self.file_path_3 = {
+            "path_name": "pictures",
+            "include": ["qr.jpg", "pic.jpg"],
+            "exclude": [],
+            "expired_time": self.default_expired_time.isoformat(),
+            "version_id": "",
+        }
+
+    def test_multiple_paths_cache(self):
+        redis_key = str(uuid.uuid4())
+        file_paths = [self.file_path_1, self.file_path_2, self.file_path_3]
+        self.redis_utils.cached_request_multiple_paths(redis_key, file_paths)
 
 
 if __name__ == "__main__":
-    redis_utils = RedisUtils()
-
-    default_expired_time = datetime.utcnow() + timedelta(days=1)
-    file_path_1 = {
-        "path_name": "videos",
-        "include": ["cat.mp4", "dog.mp4"],
-        "exclude": [],
-        "expired_time": default_expired_time.isoformat(),
-        "version_id": "",
-    }
-
-    file_path_2 = {
-        "path_name": "documents",
-        "include": ["report.pdf", "hw.pdf"],
-        "exclude": [],
-        "expired_time": default_expired_time.isoformat(),
-        "version_id": "",
-    }
-
-    file_path_3 = {
-        "path_name": "pictures",
-        "include": ["qr.jpg", "me.jpg"],
-        "exclude": [],
-        "expired_time": default_expired_time.isoformat(),
-        "version_id": "",
-    }
-
-    file_path_4 = {
-        "path_name": "pictures",
-        "include": ["me.jpg", "qr.jpg"],
-        "exclude": [],
-        "expired_time": default_expired_time.isoformat(),
-        "version_id": "",
-    }
-
-    file_paths = [file_path_1, file_path_2, file_path_3]
-
-    redis_utils.cached_request_multiple_paths("test", file_paths)
-
-    file_paths_duplicate = [file_path_1, file_path_2, file_path_4]
-
-    result = redis_utils.check_cached_multiple_paths(file_paths_duplicate)
-    print(result)
+    redis_utils = TestRedisUtils()
+    redis_utils.test_multiple_paths_cache()
